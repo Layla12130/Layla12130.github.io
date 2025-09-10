b5a558e5d41c369cc7a79942912dc08997e70ba3
Layla12130 <3180566977@qq.com>
2025-09-10 19:12:16 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+import { initThemeToggle } from "./themeToggle.js";
+import { initTypewriter } from "./typewriter.js";
+import { renderTimeline, timelineData } from "./timeline.js";
+import { renderSkills, skillsData } from "./skills.js";
+
+// 初始化所有模块
+initThemeToggle();
+initTypewriter();
+renderTimeline(timelineData);
+renderSkills(skillsData);
 
