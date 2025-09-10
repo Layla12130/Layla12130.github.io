25cdf192016466bc63131e4046727f33f70eeb78
Layla12130 <3180566977@qq.com>
2025-09-11 04:07:10 +0800

    Delete js directory

--- js/themeToggle.js
+++
@@ -1,9 +0,0 @@
-export function initThemeToggle() {
-  const button = document.getElementById("theme-toggle");
-  button.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    document.body.classList.toggle("light");
-    button.textContent = document.body.classList.contains("dark") ? "🌙" : "☀️";
-  });
-}
-
