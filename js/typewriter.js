62c660cbe9e4333a62f3b4c1035ade0fdeea10ee
Layla12130 <3180566977@qq.com>
2025-09-10 19:10:34 +0800

    Update typewriter.js

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,1 +1,33 @@
+export function initTypewriter() {
+  const textElement = document.getElementById("typewriter");
+  const texts = [
+    "你好，我是张艺桐",
+    "一名前端开发爱好者",
+    "未来的AI研究者",
+  ];
+  let textIndex = 0;
+  let charIndex = 0;
+  let deleting = false;
+
+  function type() {
+    const currentText = texts[textIndex];
+    if (!deleting) {
+      textElement.textContent = currentText.substring(0, charIndex++);
+      if (charIndex > currentText.length) {
+        deleting = true;
+        setTimeout(type, 1000);
+        return;
+      }
+    } else {
+      textElement.textContent = currentText.substring(0, charIndex--);
+      if (charIndex < 0) {
+        deleting = false;
+        textIndex = (textIndex + 1) % texts.length;
+      }
+    }
+    setTimeout(type, deleting ? 50 : 100);
+  }
+
+  type();
+}
 
